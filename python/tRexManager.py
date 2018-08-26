211bbae90cff7ab88a218f7a2d815e753f8d5042
patrickvonplaten <patrick.v.platen@gmail.com>
2018-08-26 18:32:54 +0200

    added RL agent, game and so on

---
+++ python/tRexManager.py
@@ -0,0 +1,116 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+import ipdb 
+import base64
+import io 
+import numpy as np 
+import cv2
+
+class ChromeDriver(object): 
+
+    def __init__(self, chromeExecutablePath='/usr/bin/chromedriver', tRexHtmlPath='file:///home/patrick/TRexGameRL/javascript/index.html'):
+        self.chromeExecutablePath = chromeExecutablePath
+        self.tRexHtmlPath = tRexHtmlPath
+        self.chromeOptions = ['disable-infobars']
+        self.driver = self.configureDriver(self.chromeOptions)
+
+    def configureDriver(self, chromeOptions):
+        options = Options()
+        for option in chromeOptions: 
+            options.add_argument(option)
+        
+        driver=webdriver.Chrome(executable_path=self.chromeExecutablePath, chrome_options=options)        
+        driver.set_window_position(x=-10,y=0)
+        driver.set_window_size(200, 300)
+        driver.get(self.tRexHtmlPath)
+        return driver
+
+
+class Game(object):
+    def __init__(self):
+        self.driver = ChromeDriver().driver
+
+    def getEnvironmentState(self):
+        screenAsBase64 = self.driver.get_screenshot_as_base64().encode()
+        screenAsBytes = base64.b64decode(screenAsBase64)
+        screenAsNumpyArray = cv2.imdecode(np.frombuffer(screenAsBytes, np.uint8), 0)
+        ipdb.set_trace()
+        return None
+
+    def restartGame(self):
+        self.driver.execute_script("Runner.instance_.restart()")
+
+    def pressUp(self):
+        self.driver.find_element_by_tag_name("body").send_keys(Keys.ARROW_UP)
+
+    def pressDown(self):
+        self.driver.find_element_by_tag_name("body").send_keys(Keys.ARROW_DOWN)
+
+    def isCrashed(self):
+        return self.driver.execute_script("return Runner.instance_.crashed")
+
+    def isRunning(self):
+        return self.driver.execute_script("return Runner.instance_.playing")
+
+    def getScore(self):
+        scoreArray = self.driver.execute_script("return Runner.instance_.distanceMeter.digits")
+        score = ''.join(scoreArray) 
+        return int(score)
+
+    def end(self):
+        self.driver.close()
+
+
+class Agent(object):
+
+    def __init__(self, model=None):
+        self.game = Game() 
+        self.model = model
+        self.actions = [self.jump, self.run, self.duck]
+
+    def jump(self):
+        print('jump')
+        self.game.pressUp()
+
+    def duck(self):
+        print('duck')
+        self.game.pressDown()
+
+    def run(self):
+        print('run')
+        time.sleep(.01)
+
+    def takeAction(self, code):
+        return self.actions[code].__call__()
+
+    def play(self):
+        self.jump()
+        print("Start game")
+        while(self.game.isRunning()):
+            self.processEnvironmentToAction(timeStep=0.25)
+
+    def processEnvironmentToAction(self, timeStep):
+        startTime = time.time()
+        environmentState = self.game.getEnvironmentState()
+        self.takeAction(self.model.getAction(environmentState))
+        passedTime = time.time() - startTime
+        if(passedTime < timeStep):
+            time.sleep(timeStep - passedTime)
+
+    def train(self):
+        pass
+
+class Model(object):
+
+    def __init__(self):
+        self.weights = None 
+
+    def getAction(self, environmentState):
+        from random import randint
+        return randint(0, 2)
+
+if __name__ == "__main__":
+    agent = Agent(model = Model())
+    agent.play()
