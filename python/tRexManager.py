269c5a8ecb6d79a8c3d488b84b3afef19ef86dc8
patrickvonplaten <patrick.v.platen@gmail.com>
2018-08-28 21:33:25 +0200

    start building training

--- python/tRexManager.py
+++ python/tRexManager.py
@@ -8,6 +8,7 @@ import io
 import numpy as np 
 import cv2
 import tensorflow as tf
+import ipdb
 
 class ChromeDriver(object): 
 
@@ -32,20 +33,39 @@ class ChromeDriver(object):
 class Game(object):
     def __init__(self):
         self.driver = ChromeDriver().driver
+        self.screenshotHorizontalDim = self.transfromBase64ToUint8(self.driver.get_screenshot_as_base64()).shape[1]
+        self.screenshotVerticalDimCoord = (30,141)
+        self.screenshotVerticalDim = self.screenshotVerticalDimCoord[1] - self.screenshotVerticalDimCoord[0]
+        self.discretizationLevel = 128
+        self.framesPerSample = 4
+        self.frameSamplingRate = 0.25
+        self.tempEnvironmentData = np.zeros((self.framesPerSample, self.screenshotVerticalDim, self.screenshotHorizontalDim), dtype=np.int8)
+        self.dataShape = (2*self.tempEnvironmentData.shape[0],) + self.tempEnvironmentData.shape[1:]
 
     def showEnvironmentImage(self, envImage):
         pyplot.imshow(envImage)
         pyplot.show()
 
-    def getEnvironmentState(self):
-        return self.getScore(), self.getEnvironmentImage()
-
-    def getEnvironmentImage(self):
-        screenAsBase64 = self.driver.get_screenshot_as_base64().encode()
+    def transfromBase64ToUint8(self, screenshot):
+        screenAsBase64 = screenshot.encode()
         screenAsBytes = base64.b64decode(screenAsBase64)
-        screenAsNumpyArray = cv2.imdecode(np.frombuffer(screenAsBytes, np.uint8), 0)
-        screenAsNumpyArrayShortened = (screenAsNumpyArray[30:141]/128).astype(int)
-        return screenAsNumpyArrayShortened
+        return cv2.imdecode(np.frombuffer(screenAsBytes, np.uint8), 0)
+    
+    def getEnvironmentState(self):
+        environmentData = np.zeros(self.dataShape, dtype=np.int8)
+        environmentData[:self.framesPerSample] = self.tempEnvironmentData
+        for i in range(self.framesPerSample):
+            startTime = time.time()
+            screenshot = self.driver.get_screenshot_as_base64()
+            screenshotAsUint8 = self.transfromBase64ToUint8(screenshot)
+            environmentData[i+self.framesPerSample] = (screenshotAsUint8[self.screenshotVerticalDimCoord[0]:self.screenshotVerticalDimCoord[1]]/self.discretizationLevel).astype(int)
+            endTime = time.time() - startTime
+            if(endTime < self.frameSamplingRate):
+                time.sleep(self.frameSamplingRate - endTime)
+            print('EnvState' + str(i))
+        print('Finish')
+        self.tempEnvironmentData = environmentData[self.framesPerSample:]
+        return environmentData
 
     def restartGame(self):
         self.driver.execute_script("Runner.instance_.restart()")
@@ -73,10 +93,13 @@ class Game(object):
 
 class Agent(object):
 
-    def __init__(self, model=None):
+    def __init__(self, model, mode, epochToCollectData):
         self.game = Game() 
         self.model = model
         self.actions = [self.jump, self.run, self.duck]
+        self.mode = mode 
+        self.epochToCollectData = epochToCollectData
+        self.timeStep = 0.25
 
     def jump(self):
         print('jump')
@@ -88,27 +111,64 @@ class Agent(object):
 
     def run(self):
         print('run')
-        time.sleep(.01)
+        pass
 
     def takeAction(self, code):
         return self.actions[code].__call__()
+
+    def run(self): 
+        if(self.mode == 'play'):
+            self.play()
+        if(self.mode == 'train'):
+            self.train()
 
     def play(self):
         self.jump()
         print("Start game")
         while(self.game.isRunning()):
-            self.processEnvironmentToAction(timeStep=0.25)
+            self.processEnvironmentToAction()
 
-    def processEnvironmentToAction(self, timeStep):
-        startTime = time.time()
-        score, environment = self.game.getEnvironmentState()
-        self.takeAction(self.model.getAction(environment))
-        passedTime = time.time() - startTime
-        if(passedTime < timeStep):
-            time.sleep(timeStep - passedTime)
+    def processEnvironmentToAction(self):
+        environment = self.game.getEnvironmentState()
+        actionCode = self.model.getAction(environment)
+        self.takeAction(actionCode)
+        print('Action')
+        return actionCode, environment
 
     def train(self):
-        pass
+        trainingData = self.collectTrainingData()
+
+    def collectTrainingData(self):
+        self.jump()
+        trainingData = [[None, None, None]]
+        oldEnvironment = None
+        for i in range(self.epochToCollectData+1):
+            actionCode, environment = self.processEnvironmentToAction()
+            reward = self.getReward(actionCode)
+            if(self.game.isCrashed()):
+                self.game.restartGame()
+            sample = []
+            sample.append(environment)
+            sample.append(actionCode)
+            sample.append(reward)
+            sample.append(None)
+            trainingData[i-1].append(environment)
+            trainingData.append(sample)
+            print(i)
+            if(i==2):
+                ipdb.set_trace()
+        trainingData = trainingData[1:-1]
+        return trainingData
+            
+    def getReward(self, actionCode):
+        if(self.game.isCrashed()):
+            return -100
+        elif(self.actions[actionCode].__name__ == 'jump'):
+            return -5
+        elif(self.actions[actionCode].__name__ == 'duck'):
+            return -3 
+        else: 
+            return 1
 
 class Model(object):
 
@@ -120,5 +180,5 @@ class Model(object):
         return randint(0, 2)
 
 if __name__ == "__main__":
-    agent = Agent(model = Model())
-    agent.play()
+    agent = Agent(model = Model(),mode='train', epochToCollectData=40)
+    agent.run()
