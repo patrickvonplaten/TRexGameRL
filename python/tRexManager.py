cced612491271d9afb885b30a4f40c4f87e12674
Kilian Pfeiffer <kilian.pfeiffer@rwth-aachen.de>
2018-09-03 22:44:00 +0200

    Make agent independent of game.

--- python/tRexManager.py
+++ python/tRexManager.py
@@ -8,13 +8,12 @@ from imageio import imwrite
 import os
 import time
 import base64
-import io 
-import numpy as np 
+import io
+import numpy as np
 import sys
 import cv2
-import tensorflow as tf
 import ipdb
-import os
+import argparse
 
 WINDOW_SIZE = '400,300'
 CHROME_EXECUTABLEPATH = '/usr/bin/chromedriver' 
@@ -27,7 +26,7 @@ class ChromeDriver(object):
 
     def __init__(self):
         chromeOptions = ['disable-infobars', '--window-size=%s' % WINDOW_SIZE]
-        chromeOptions.append('--headless')
+#        chromeOptions.append('--headless')
         self.driver = self.configureDriver(chromeOptions)
 
     def configureDriver(self, chromeOptions):
@@ -40,8 +39,19 @@ class ChromeDriver(object):
         driver.get(TREX_HTML_PATH)
         return driver
 
+
+class Action(object):
+    def __init__(self, action_fn, reward, code=None):
+        self.action = action_fn
+        self.code = code
+        self.reward = reward
+    def __call__(self):
+        if self.code:
+            print(self.code)
+        self.action()
+
 class Game(object):
-    def __init__(self, framesPerSample=4, frameSamplingRate=0.1):
+    def __init__(self, framesPerSample, frameSamplingRate):
         self.driver = ChromeDriver().driver
         self.screenshotShape = self.transfromBase64ToUint8(self.driver.get_screenshot_as_base64()).shape
         self.framesPerSample = framesPerSample
@@ -56,7 +66,7 @@ class Game(object):
         screenAsBase64 = screenshot.encode()
         screenAsBytes = base64.b64decode(screenAsBase64)
         return cv2.imdecode(np.frombuffer(screenAsBytes, np.uint8), 0)
-    
+
     def getEnvironmentState(self):
         environmentData = np.zeros(self.environmentDataShape, dtype=np.uint8)
         for i in range(self.framesPerSample):
@@ -69,14 +79,38 @@ class Game(object):
                 time.sleep(self.frameSamplingRate - endTime)
         return environmentData
 
-    def restartGame(self):
-        return self.driver.execute_script("Runner.instance_.restart()")
+    def isCrashed(self):
+        raise NotImplementedError()
 
-    def pressUp(self):
+    def isRunning(self):
+        raise NotImplementedError()
+
+    def getState(self, action):
+        raise NotImplementedError()
+
+    def restart(self):
+        raise NotImplementedError()
+
+    def end(self):
+        raise NotImplementedError()
+
+
+class TRexGame(Game):
+    def __init__(self, framesPerSample=4, frameSamplingRate=0.1):
+        super().__init__(framesPerSample, frameSamplingRate)
+        jump = Action(self._pressUp, -5, "jump")
+        duck = Action(self._pressDown, -3, "duck")
+        run = Action(lambda: None, 1, "run")
+        self.actions = [jump, duck, run]
+
+    def _pressUp(self):
         return self.driver.find_element_by_tag_name("body").send_keys(Keys.ARROW_UP)
 
-    def pressDown(self):
+    def _pressDown(self):
         return self.driver.find_element_by_tag_name("body").send_keys(Keys.ARROW_DOWN)
+
+    def restart(self):
+        return self.driver.execute_script("Runner.instance_.restart()")
 
     def isCrashed(self):
         return self.driver.execute_script("return Runner.instance_.crashed")
@@ -93,6 +127,13 @@ class Game(object):
     def end(self):
         self.driver.quit()
 
+    def getReward(self, actionCode):
+        if(self.isCrashed()):
+            return -100
+        else:
+            action = self.actions[actionCode]
+            return action.reward
+
 
 class Agent(object):
 
@@ -104,26 +145,16 @@ class Agent(object):
         self.actionCode = ['jump', 'run','duck']
         self.trainingData = None
         self.pathToImageFolder = PATH_TO_IMAGE_FOLDER
-
-    def jump(self):
-        print('jump')
-        self.game.pressUp()
-
-    def duck(self):
-        print('duck')
-        self.game.pressDown()
-
-    def run(self):
-        print('run')
-        pass
+        if not os.path.isdir(self.pathToImageFolder):
+            os.mkdir(self.pathToImageFolder)
 
     def takeAction(self, code):
         if(self.actionCode[code] == 'jump'):
-            return self.jump()
+            return self.game.actions[0]()
         elif(self.actionCode[code] == 'run'):
-            return self.run()
+            return self.game.actions[1]()
         elif(self.actionCode[code] == 'duck'):
-            return self.duck()
+            return self.game.actions[2]()
 
     def execute(self): 
         if(self.mode == 'play'):
@@ -132,7 +163,7 @@ class Agent(object):
             self.train()
 
     def play(self):
-        self.jump()
+        self.takeAction(0)
         while(self.game.isRunning()):
             self.processEnvironmentToAction()
 
@@ -147,16 +178,16 @@ class Agent(object):
         trainingData = self.collectTrainingData()
 
     def collectTrainingData(self):
-        self.jump()
+        self.takeAction(0)
         self.trainingData = [[None, None, None]]
         oldEnvironment = None
         for i in range(self.epochToCollectData+2):
             print('iter start' + str(i), flush=True)
             actionCode, environment = self.processEnvironmentToAction()
             print('iter end' + str(i), flush=True)
-            reward = self.getReward(actionCode)
+            reward = self.game.getReward(actionCode)
             if(self.game.isCrashed()):
-                self.game.restartGame()
+                self.game.restart()
             sample = []
             sample.append(environment)
             sample.append(actionCode)
@@ -175,21 +206,13 @@ class Agent(object):
             for j in range(4):
                 imageToSave = self.trainingData[i][0][j]
 #                ipdb.set_trace()
-                imwrite(os.path.join(self.pathToImageFolder,'env_' + str(i) + '_' + str(j+4) + '.jpg'), imageToSave)
+                image_name = 'env_{}_{}.jpg'.format(i, j+4)
+                imwrite(os.path.join(self.pathToImageFolder, image_name), imageToSave)
 
-    def getReward(self, code):
-        if(self.game.isCrashed()):
-            return -100
-        elif(self.actionCode[code] == 'jump'): 
-            return -5
-        elif(self.actionCode[code] == 'duck'): 
-            return -3 
-        elif(self.actionCode[code] == 'run'): 
-            return 1
 
 if __name__ == "__main__":
     model = TFRexModel()
-    game = Game()
+    game = TRexGame()
     agent = Agent(game = game, model = model,mode='train', epochToCollectData=20)
     agent.execute()
     agent.saveEnvironmentScreenshots()
