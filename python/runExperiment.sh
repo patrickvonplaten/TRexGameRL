b200de868fb8e745bcab839c56f81b2ae77b1424
Patrick von Platen <platen@i6.informatik.rwth-aachen.de>
2018-10-28 17:54:42 +0100

    adapted run function

--- python/runExperiment.sh
+++ python/runExperiment.sh
@@ -9,6 +9,7 @@ if [ $# -eq 0 ]
 fi
 
 curPath=$(pwd)
+mainPath=${curPath}/main.py
 setupPath=${curPath}/experiments/${nameOfExperiment}
 mkdir ${setupPath}
 mkdir ${setupPath}/log
@@ -16,15 +17,7 @@ mkdir ${setupPath}/models
 cd ${setupPath}
 
 echo "${HOSTNAME}" > started_on_host.txt
-cp ${curPath}/main.py ${setupPath} 
-sed -i "/CUR_PATH = os.*/d" ${setupPath}/main.py
-sed -i "/PATH_TO_TREX_MODULES =*/c\PATH_TO_TREX_MODULES = \'\/u\/platen\/TRexGameRL\/python\/src\'" ${setupPath}/main.py
-sed -i "/PATH_TO_MODELS =*/c\PATH_TO_MODELS = \'${setupPath}\/models\'" ${setupPath}/main.py
-sed -i "/PATH_TO_LOG =*/c\PATH_TO_LOG = \'${setupPath}\/log\'" ${setupPath}/main.py
-cp ${curPath}/doit.sh ${setupPath}
-
+cp ${curPath}/training.config ${setupPath} 
 cd ${setupPath}
-#qint doit.sh
-#qint -t blabla.1 doit.sh tRexTraining
-./main.py
+python ${mainPath}
 
