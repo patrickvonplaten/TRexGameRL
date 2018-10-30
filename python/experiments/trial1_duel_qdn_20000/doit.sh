b585cede9b18ce9f2908d35f04a271b7d3bb8f00
patrickvonplaten <patrick.v.platen@gmail.com>
2018-10-30 11:42:04 +0000

    updatet gitignore

--- python/experiments/trial1_duel_qdn_20000/doit.sh
+++
@@ -1,7 +0,0 @@
-sequential tRexTraining(qsub="-hard -l h_vmem=15G -l h_rt=80:00:00"):
-	curPath=$(pwd)
-	pythonExec='/u/platen/virtualenvironment/tRex/bin/python'
-	echo "$(ls /usr/bin/google-chrome)"
-	tRexExec=${curPath}/main.py
-#	${pythonExec} ${tRexExec} 
-	python ${tRexExec} 
